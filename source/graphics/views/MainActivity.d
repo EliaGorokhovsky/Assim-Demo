5f28781e041371a26c818cfc588d5217032130fe
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2018-03-26 19:21:54 -0600

    Added scale

--- source/graphics/views/MainActivity.d
+++ source/graphics/views/MainActivity.d
@@ -2,8 +2,10 @@ module graphics.views.MainActivity;
 
 import std.algorithm;
 import std.array;
+import std.conv;
 import std.math;
 import d2d;
+import graphics.Constants;
 import logic.demo.PointGetter;
 
 /**
@@ -13,11 +15,14 @@ class MainActivity : Activity {
 
     PointGetter[] pointGetters; ///Gets points to graph
     double[][] drawablePoints; ///All the points to be on the screen
-    AxisAlignedBoundingBox!(int, 2) location;
-    dVector xScale;
-    dVector yScale;
+    AxisAlignedBoundingBox!(int, 2) location; ///The location of the graph element
+    dVector xScale; ///The range of x(time) values represented on the graph
+    uint xTicks = 5; ///How many tick marks are represented on the time scale
+    dVector yScale; ///The range of y values represented on the graph
+    uint yTicks = 5; ///How many tick marks are represented on the y scale
     iVector[][] toDraw; ///The points to be drawn in a frame
-    Color[] colors;
+    Color[] colors; ///The colors of each of the curves to draw
+    Font font; ///The font with which to draw text
 
     /**
      * Constructor for the main activity
@@ -25,6 +30,7 @@ class MainActivity : Activity {
      */
     this(Display display, PointGetter[] pointGetters, AxisAlignedBoundingBox!(int, 2) location, dVector xScale, dVector yScale) { 
         super(display);
+        this.font = new Font("res/fonts/OpenSansRegular.ttf", 100);
         this.pointGetters = pointGetters;
         this.location = location;
         this.xScale = xScale;
@@ -84,7 +90,7 @@ class MainActivity : Activity {
     override void draw() {
         this.container.renderer.clear(PredefinedColor.WHITE);
         this.container.renderer.fill(this.location, PredefinedColor.LIGHTGREY);
-        //import std.stdio; writeln(toDraw);
+        //Draw curves
         foreach(curve; 0..this.drawablePoints.length) {
             if(this.toDraw[curve].length > 1) {
                 foreach(i; 0..this.toDraw[curve].length - 1) {
@@ -96,5 +102,30 @@ class MainActivity : Activity {
                 this.container.renderer.draw(this.toDraw[curve][0]);
             }
         }
+        //Draw scale
+        //TODO: Avoid hardcoding location
+        this.container.renderer.fill(new AxisAlignedBoundingBox!(int, 2)(this.location.initialPoint.x - 2, this.location.initialPoint.y - 2, 4, this.location.extent.y + 4));
+        this.container.renderer.fill(new AxisAlignedBoundingBox!(int, 2)(this.location.bottomLeft.x - 2, this.location.bottomLeft.y - 2, this.location.extent.x + 4, 4));
+        foreach(i; 0..this.xTicks) {
+            double value =  this.xScale.x + i * (this.xScale.y - this.xScale.x) / (this.xTicks - 1);
+            string textValue = value.to!string;
+            string roundedTextValue = value.trunc.to!string;
+            Texture text = new Texture(this.font.renderTextSolid(textValue), this.container.renderer);
+            int xCenter = this.location.bottomLeft.x + i * this.location.extent.x / (this.xTicks - 1);
+            this.container.renderer.fill(new AxisAlignedBoundingBox!(int, 2)(xCenter - 2, this.location.bottomLeft.y - logicalSize.y / 80, 4, logicalSize.y / 40));
+            this.container.renderer.copy(text, new iRectangle(xCenter - 10 * (roundedTextValue.length + 2), this.location.bottomLeft.y + logicalSize.y / 80, 20 * textValue.length, 40));
+        }
+        foreach(i; 0..this.yTicks) {
+            double value = this.yScale.y - i * (this.yScale.y - this.yScale.x) / (this.yTicks - 1);
+            string textValue = value.to!string;
+            Texture text = new Texture(this.font.renderTextSolid(textValue), this.container.renderer);
+            int yCenter = this.location.initialPoint.y + i * this.location.extent.y / (this.yTicks - 1);
+            this.container.renderer.fill(new AxisAlignedBoundingBox!(int, 2)(this.location.initialPoint.x - logicalSize.y / 80, yCenter - 2, logicalSize.y / 40, 4));
+            this.container.renderer.copy(text, new iRectangle(this.location.initialPoint.x - logicalSize.y / 80 - 20 * textValue.length, yCenter - 20, 20 * textValue.length, 40));
+        }
+        Texture text = new Texture(this.font.renderTextSolid("t"), this.container.renderer);
+        this.container.renderer.copy(text, new iRectangle(this.location.bottomRight.x + logicalSize.x / 80, this.location.bottomRight.y - 20, 20, 40));
+        text = new Texture(this.font.renderTextSolid("x"), this.container.renderer);
+        this.container.renderer.copy(text, new iRectangle(this.location.initialPoint.x - 10, this.location.initialPoint.y - 40 - logicalSize.y / 80, 20, 40));
     }
 }
